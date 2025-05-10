4c2195e94eae5ce678fe456a7db8d6eb4008d087
Yacine Bakkoury <yacinebakk@gmail.com>
2025-05-10 15:02:34 +0200

    clock stops when all visitors have left

--- UI/gui.py
+++ UI/gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk  # Pillow is needed for JPG images
 import threading
+import sys
 
 import Time_manager as time_manager
 
@@ -54,27 +55,72 @@ class ParkGUI:
         self.visitor_threads = visitor_threads
 
         self.visitor_items = {}  # Dictionary to store each visitor's dot
+        
+        # Flag to track when all visitors have left
+        self.all_visitors_exited = False
+        # Time when all visitors exited
+        self.exit_time = None
 
         self.update_gui()
+        
+        # Start checking if all visitors have exited
+        self.check_all_visitors_exited()
 
     def update_gui(self):
         for visitor in self.visitor_threads:
+            # Only filter visitors who haven't entered yet
+            if not visitor.entered:
+                continue
+
             vid = visitor.visitor_id
-            # Get the visitor's current coordinates (assumed to be a tuple)
             x, y = visitor.coords
-            # Convert to integers (in case they're floats)
-            # x, y = int(x), int(y)
+            
             if vid not in self.visitor_items:
-                # Create a new dot (oval) for the visitor if it doesn't exist
                 dot = self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="red")
                 self.visitor_items[vid] = dot
             else:
                 # Update the dot's position on the canvas
                 self.canvas.coords(self.visitor_items[vid], x - 5, y - 5, x + 5, y + 5)
+                
+                # Change color if visitor is leaving/not running
+                if not visitor.running:
+                    self.canvas.itemconfig(self.visitor_items[vid], fill="blue")
+                else:
+                    self.canvas.itemconfig(self.visitor_items[vid], fill="red")
+                
         # Schedule the next update after 100 milliseconds
         self.master.after(100, self.update_gui)
         self.time_label.config(text=self.time_manager.get_current_time())
         self.day_label.config(text=self.time_manager.get_current_day())
+    
+    def check_all_visitors_exited(self):
+        """Check if all visitors have exited the park and close if they have"""
+        # Skip check if we haven't closed the park yet
+        if int(self.time_manager.get_current_time().split(":")[0]) < 22:
+            self.master.after(5000, self.check_all_visitors_exited)
+            return
+            
+        # Count visitors still running
+        active_visitors = sum(1 for visitor in self.visitor_threads if visitor.running)
+        
+        # If all visitors have exited but we haven't set the flag yet
+        if active_visitors == 0 and not self.all_visitors_exited:
+            self.all_visitors_exited = True
+            self.exit_time = self.time_manager.get_current_time()
+            print(f"All visitors have exited the park at {self.exit_time}. Simulation will end in 20 seconds.")
+            
+            # Schedule the final shutdown after 20 seconds
+            self.master.after(20000, self.shutdown_simulation)
+            return
+            
+        # If there are still visitors, check again in 2 seconds
+        if not self.all_visitors_exited:
+            self.master.after(2000, self.check_all_visitors_exited)
+    
+    def shutdown_simulation(self):
+        """Shutdown the simulation after all visitors have exited"""
+        print(f"Closing simulation after 20-second delay. Final time: {self.time_manager.get_current_time()}")
+        self.master.destroy()
 
 def start_gui(visitor_threads, time_manager):
     root = tk.Tk()
